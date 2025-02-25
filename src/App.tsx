b077e4d67c1ce009f0a1c72f1164d6cb58b7d246
jaysson18 <jahaka999@gmail.com>
2025-02-25 13:33:34 +0200

    kuvat jne lisätty

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
-import React, {useState} from 'react';
 import {StatusBar} from 'expo-status-bar';
-import {SafeAreaView, StyleSheet, Text, Button} from 'react-native';
+import {Platform, SafeAreaView, StyleSheet} from 'react-native';
+import Home from './views/Home';
 
 const App = () => {
-  const [text1, setText1] = useState('Moro!');
-  const [text2, setText2] = useState('Tää skulaa?');
-
-  const changeText1 = () => {
-    setText1((prevText) => (prevText === 'Moro!' ? 'Hello!' : 'Moro!'));
-  };
-
-  const changeText2 = () => {
-    setText2((prevText) =>
-      prevText === 'Tää skulaa?' ? 'Does this work?' : 'Tää skulaa?',
-    );
-  };
-
   console.log('App loaded!');
   return (
     <SafeAreaView style={styles.container}>
-      <Text>{text1}</Text>
-      <Button title="Change Text 1" onPress={changeText1} />
-      <Text>{text2}</Text>
-      <Button title="Change Text 2" onPress={changeText2} />
+      <Home />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -32,8 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'ios' ? 0 : 30,
   },
 });
 
