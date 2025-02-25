0c0cf7cd887b05a4245b0743775d153ec0e30f09
jaysson18 <jahaka999@gmail.com>
2025-02-25 09:57:35 +0200

    Ensimmäinen

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import {StatusBar} from 'expo-status-bar';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Moro!</Text>
+      <Text>Tää skulaa?</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
