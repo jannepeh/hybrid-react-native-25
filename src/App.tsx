9f84c44519628e6ab8e408117c22531afa8bc8e5
jaysson18 <jahaka999@gmail.com>
2025-02-25 10:12:09 +0200

    first edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import React, {useState} from 'react';
 import {StatusBar} from 'expo-status-bar';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, Button} from 'react-native';
 
 const App = () => {
+  const [text1, setText1] = useState('Moro!');
+  const [text2, setText2] = useState('Tää skulaa?');
+
+  const changeText1 = () => {
+    setText1((prevText) => (prevText === 'Moro!' ? 'Hello!' : 'Moro!'));
+  };
+
+  const changeText2 = () => {
+    setText2((prevText) =>
+      prevText === 'Tää skulaa?' ? 'Does this work?' : 'Tää skulaa?',
+    );
+  };
+
+  console.log('App loaded!');
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Moro!</Text>
-      <Text>Tää skulaa?</Text>
+      <Text>{text1}</Text>
+      <Button title="Change Text 1" onPress={changeText1} />
+      <Text>{text2}</Text>
+      <Button title="Change Text 2" onPress={changeText2} />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
