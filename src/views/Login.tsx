489b845c345d3560e99e329378a7faa2966cf010
jaysson18 <jahaka999@gmail.com>
2025-02-27 10:33:55 +0200

    register form touch ups

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
 import {Button} from '@rneui/base';
+import {ScrollView} from 'react-native';
 
 const Login = () => {
   const [displayRegister, setDisplayRegister] = useState(false);
@@ -12,10 +13,16 @@ const Login = () => {
 
   return (
     <>
-      {displayRegister ? <RegisterForm /> : <LoginForm />}
-      <Button onPress={toggleRegister}>
-        or {displayRegister ? 'login' : 'register'}?
-      </Button>
+      <ScrollView>
+        {displayRegister ? (
+          <RegisterForm setDisplayRegister={setDisplayRegister} />
+        ) : (
+          <LoginForm />
+        )}
+        <Button onPress={toggleRegister}>
+          or {displayRegister ? 'login' : 'register'}?
+        </Button>
+      </ScrollView>
     </>
   );
 };
