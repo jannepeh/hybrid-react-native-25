5e3c828a694efcf9f42013aa166ea6819c9e3546
jaysson18 <jahaka999@gmail.com>
2025-02-27 10:57:17 +0200

    register form touch ups

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,15 +1,21 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
 import {Button} from '@rneui/base';
 import {ScrollView} from 'react-native';
+import {useUserContext} from '../hooks/ContextHooks';
 
 const Login = () => {
   const [displayRegister, setDisplayRegister] = useState(false);
+  const {handleAutoLogin} = useUserContext();
 
   const toggleRegister = () => {
     setDisplayRegister(!displayRegister);
   };
+
+  useEffect(() => {
+    handleAutoLogin();
+  }, []);
 
   return (
     <>
